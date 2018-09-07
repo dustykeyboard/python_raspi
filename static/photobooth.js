6bc902199022540fdb440bde5e595c3761179ae3
Philip Boardman <dustykeyboard@brd.mn>
2018-09-07 15:48:09 +1000

    JS Refreshing from a JSON endpoint

--- static/photobooth.js
+++ static/photobooth.js
@@ -1,5 +1,19 @@
-console.log('Photobooth')
+let cache = []
 
-window.onload = function() {
-	setTimeout(() => console.log('delay!'), 15000)
+function refresh() {
+	fetch('latest.json').then(response => response.json()).then(({ photos }) => {
+		let update = false
+		for (var i = photos.length - 1; i >= 0; i--) {
+			if (cache.indexOf(photos[i]) === -1) {
+				update = true
+				cache.unshift(photos[i])
+			}
+		}
+		if (!update) return
+
+		document.querySelector('#photos').innerHTML = cache.map(url => `<img src='${url}' />`).join('')
+	})
 }
+
+setInterval(refresh, 5000)
+window.onload = refresh
